e3b11fbb583137e448e0d843f07c21687edfccd0
MarkovicLazar <lazar.petlja@gmail.com>
2023-08-06 00:39:26 +0200

    Added login flyout

--- js/main.js
+++ js/main.js
@@ -1,47 +1,64 @@
 window.onload = () => {
 	const startButton = document.getElementById('start');
-	const containter = document.getElementById('container');
 	const startBackground = document.getElementById('start-pozadina');
 	const signIn = document.getElementById('sign-in');
-	const signInBackground = document.getElementById('sign-in-pozadina');
+	const blurBackground = document.getElementById('blur-background');
 	let signInShown = false;
+	let loginShown = false;
 	const joinButton = document.getElementById('join');
 	const body = document.body;
+	const loginButton = document.getElementById('nav-login');
+	const login = document.getElementById('login');
+	const forgotPassword = document.getElementById('forgot-password');
 	startButton.onmouseenter = () => {
 		startBackground.style.width = '45px';
 	}
 	startButton.onmouseleave = () => {
 		startBackground.style.width = '190px';
 	}
-	signInBackground.onclick = (event) => {
+	blurBackground.onclick = (event) => {
 		if (signInShown && !signIn.contains(event.target)) {
-			signInBackground.classList.add('hide');
+			blurBackground.classList.add('hide');
 			signIn.classList.add('hide');
 			body.classList.remove('stop-scroll');
 			setTimeout(() => {
 				signIn.style.opacity = '0%';
-				signInBackground.style.opacity = '100%';
+				blurBackground.style.opacity = '100%';
 				signIn.style.display = 'none';
-				signInBackground.style.display = 'none';
-				signInBackground.classList.remove('hide');
+				blurBackground.style.display = 'none';
+				blurBackground.classList.remove('hide');
 				signIn.classList.remove('hide');
 				signInShown = false;
+			}, 1000);
+		}
+		else if (loginShown && !login.contains(event.target)) {
+			blurBackground.classList.add('hide');
+			login.classList.add('hide');
+			body.classList.remove('stop-scroll');
+			setTimeout(() => {
+				login.style.opacity = '0%';
+				blurBackground.style.opacity = '100%';
+				login.style.display = 'none';
+				blurBackground.style.display = 'none';
+				blurBackground.classList.remove('hide');
+				login.classList.remove('hide');
+				loginShown = false;
 			}, 1000);
 		}
 	}
 	startButton.onclick = () => {
 		if(!signInShown) {
 			signIn.style.display = 'block';
-			signInBackground.style.display = 'block';
-			signInBackground.classList.add('show');
+			blurBackground.style.display = 'block';
+			blurBackground.classList.add('show');
 			signIn.classList.add('show');
 			body.classList.add('stop-scroll');
 			setTimeout(() => {
 				signInShown = true;
 				signIn.style.opacity = '100%';
-				signInBackground.style.opacity = '100%';
+				blurBackground.style.opacity = '100%';
 				signIn.classList.remove('show');
-				signInBackground.classList.remove('show');
+				blurBackground.classList.remove('show');
 			}, 1000);
 		}
 	}
@@ -50,7 +67,41 @@ window.onload = () => {
 			countryError.style.display = "block";
 		}
 	}
+	loginButton.onclick = () => {
+		if(!loginShown) {
+			login.style.display = 'block';
+			blurBackground.style.display = 'block';
+			blurBackground.classList.add('show');
+			login.classList.add('show');
+			body.classList.add('stop-scroll');
+			setTimeout(() => {
+				loginShown = true;
+				login.style.opacity = '100%';
+				blurBackground.style.opacity = '100%';
+				login.classList.remove('show');
+				blurBackground.classList.remove('show');
+			}, 1000);
+		}
+	}
+	document.getElementById('create-account').onclick = () => {
+		login.classList.add('hide');
+		setTimeout(() => {
+			login.style.opacity = '0%';
+			login.style.display = 'none';
+			login.classList.remove('hide');
+			loginShown = false;
+		}, 1000);
+		signIn.style.display = 'block';
+		signIn.classList.add('show');
+		setTimeout(() => {
+			signInShown = true;
+			signIn.style.opacity = '100%';
+			signIn.classList.remove('show');
+		}, 1000);
+	}
+	document.getElementById('already-have-account').onclick = () => {
 
+	}
 }
 
 
